e5d0d65a4372ee12aa954d60a7ac4640040eb8bf
rimever <mot_siva_ordin@hotmail.com>
2020-04-21 22:01:57 +0900

    適当なDataGridを作成してコミット。

---
+++ _DataGridSandbox/DataGridSandbox/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridSandbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Item
+        {
+            public string Name { get; set; }
+            public bool IsFruits { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var data = new ObservableCollection<Item>()
+            {
+                new Item()
+                {
+                    Name = "みかん",
+                    IsFruits = true
+                },
+                new Item()
+                {
+                    Name = "キャベツ",
+                    IsFruits = false
+                }
+            };
+            this.dataGrid.ItemsSource = data;
+        }
+    }
+}
